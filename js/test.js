aafd8b0521728d77dc28302b1d6567411915d4ca
차경훈 <cksuwjr@naver.com>
2022-01-27 15:03:34 +0900

    내셔널지오그래픽 카테고리 메뉴바 따라잡기

--- js/test.js
+++ js/test.js
@@ -1,12 +1,4 @@
-$ ( '.layer_type .sub_depth0 li'). on ({
-    'mouseover': function () {
-        $ (this) .find ( '> ul'). stop (true, true) .fadeIn ( 'fast');
-        $ (this) .find ( '> a'). addClass ( 'active');
-        //console.log('open ');
-    },
-    'mouseleave': function () {
-        $ (this) .find ( '> ul'). stop (true, true) .fadeOut ( 'fast');
-        $ (this) .find ( '> a'). removeClass ( 'active');
-        //console.log('hide ');
-    }
+$(document).on('mouseover', '#cssmenu1 ul li', function(){
+    $('#cssmenu1 sub_depth1').slideDown(200);
 });
+
