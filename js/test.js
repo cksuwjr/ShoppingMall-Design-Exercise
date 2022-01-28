5d4ec6b484ca5e92dcead8196a798269e587c6a2
cksuwjr <63836325+cksuwjr@users.noreply.github.com>
2022-01-28 11:40:15 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,12 @@
-$(document).on('mouseover', '#cssmenu1 ul li', function(){
-    $('#cssmenu1 sub_depth1').slideDown(200);
+$(document).ready(function(){
+  $('#cssmenu1 ul li').click(function(){
+    if($(this).hasClass('open')){
+      $('#cssmenu1 ul li').removeClass('open');
+    }else{
+      $('#cssmenu1 ul li').removeClass('open');
+      $(this).addClass('open');
+      $(this).children('ul').css("display", "block");
+      $(this).children('ul').css("z-index", "100");
+    }
+  });
 });
-
