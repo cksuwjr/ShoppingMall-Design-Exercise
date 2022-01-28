38ee97a8e3d7d7e7e7709fd66f106d4d6190c011
cksuwjr <63836325+cksuwjr@users.noreply.github.com>
2022-01-28 12:04:42 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,12 +1,67 @@
 $(document).ready(function(){
   $('#cssmenu1 ul li').click(function(){
-    if($(this).hasClass('open')){
-      $('#cssmenu1 ul li').removeClass('open');
+    
+    var element = $(this);
+    if(element.hasClass('open')){
+      element.removeClass('open');
     }else{
       $('#cssmenu1 ul li').removeClass('open');
-      $(this).addClass('open');
-      $(this).children('ul').css("display", "block");
-      $(this).children('ul').css("z-index", "100");
+      element.addClass('open');
+      element.children('ul').slideDown(200);
     }
   });
 });
+
+
+
+/*
+(function($){
+$(document).ready(function(){
+
+$('#cssmenu li.active').addClass('open').children('ul').show();
+	$('#cssmenu li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp(200);
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown(200);
+			element.siblings('li').children('ul').slideUp(200);
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp(200);
+		}
+	});
+
+});
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+
+$('#cssmenu1 li.active').addClass('open').children('ul').show();
+	$('#cssmenu1 li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp(200);
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown(200);
+			element.siblings('li').children('ul').slideUp(200);
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp(200);
+		}
+	});
+
+});
+})(jQuery);
+*/
